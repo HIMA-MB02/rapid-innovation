7620761b4b527f71175446df8396b49cc5ca4522
HIMA-MB02 <himanshu.ganpa@gmail.com>
2021-08-05 00:17:39 +0530

    route security

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import { Redirect, Switch, Route } from 'react-router-dom';
-import { LandingPage, AccountPage, PagesPage, DocumentationPage } from '../pages';
+import { Redirect, Switch, Route, useLocation } from 'react-router-dom';
+import {
+  LandingPage,
+  AccountPage,
+  PagesPage,
+  DocumentationPage,
+} from '../pages';
 
 export const routes = {
-    home: '/',
-    landingPage: '/landing-page',
-    accountPage: '/account-page',
-    pagesPage: '/pages-page',
-    documentationPage: '/documentation-page'
-}
+  home: '/',
+  landingPage: '/landing-page',
+  accountPage: '/account-page',
+  pagesPage: '/pages-page',
+  documentationPage: '/documentation-page',
+};
 
 const Routes = () => {
-    return (
-        <Switch>
-            <Redirect exact from='/' to={routes.landingPage} />
-            <Route exact component={LandingPage} path={routes.landingPage} />
-            <Route exact component={AccountPage} path={routes.accountPage} />
-            <Route exact component={PagesPage} path={routes.pagesPage} />
-            <Route exact component={DocumentationPage} path={routes.documentationPage} />
-        </Switch>
-    );
+  const location = useLocation();
+  const isRouteValid = (pathname) => {
+    let isValid = false;
+    Object.keys(routes).forEach((routeKey) => {
+      if (routes[routeKey] === pathname) isValid = true;
+    });
+    return isValid;
+  };
+  return (
+    <Switch>
+      <Redirect exact from='/' to={routes.landingPage} />
+      <Route exact component={LandingPage} path={routes.landingPage} />
+      {isRouteValid(location.pathname) ? (
+        <>
+          <Route exact component={AccountPage} path={routes.accountPage} />
+          <Route exact component={PagesPage} path={routes.pagesPage} />
+          <Route
+            exact
+            component={DocumentationPage}
+            path={routes.documentationPage}
+          />
+        </>
+      ) : (
+        <Redirect to='/' />
+      )}
+    </Switch>
+  );
 };
 
 export default Routes;
